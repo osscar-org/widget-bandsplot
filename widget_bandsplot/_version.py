4b5484b7e7a1ff378efacb8b085c1a916ee7678c
Dou <dou.du.epfl@gmail.com>
2021-09-10 23:40:53 +0200

    release v0.2.4

--- widget_bandsplot/_version.py
+++ widget_bandsplot/_version.py
@@ -1,5 +1,5 @@
 # Module version
-version_info = (0, 2, 3, 'final')
+version_info = (0, 2, 4, 'final')
 
 # Module version stage suffix map
 _specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
