98264ebdf72990d2cfafa5f614c370412f64dbed
Jusong Yu <jusong.yeu@gmail.com>
2022-08-11 12:26:43 +0000

    Add basic CI
    Update setup builder and add schema test
    
    Fix dependencies
    
    Remove voila test from pytest list
    
    Add PDOS schema
    
    pre-commit
    
    Example json checked by pre-commit

--- setup.py
+++ setup.py
@@ -1,48 +1,55 @@
-from __future__ import print_function
-from setuptools import setup, find_packages
 import os
-from os.path import join as pjoin
 from distutils import log
+from os.path import join as pjoin
 from pathlib import Path
 
 from jupyter_packaging import (
-    create_cmdclass,
-    install_npm,
-    ensure_targets,
     combine_commands,
+    create_cmdclass,
+    ensure_targets,
     get_version,
+    install_npm,
 )
-
+from setuptools import find_packages, setup
 
 here = os.path.dirname(os.path.abspath(__file__))
 
 log.set_verbosity(log.DEBUG)
-log.info('setup.py entered')
-log.info('$PATH=%s' % os.environ['PATH'])
+log.info("setup.py entered")
+log.info("$PATH=%s" % os.environ["PATH"])
 
-name = 'widget_bandsplot'
-LONG_DESCRIPTION = 'A Jupyter widget to plot bandstructures.'
+name = "widget_bandsplot"
+LONG_DESCRIPTION = "A Jupyter widget to plot bandstructures."
 
 # Get widget_bandsplot version
-version = get_version(pjoin(name, '_version.py'))
+version = get_version(pjoin(name, "_version.py"))
 
-js_dir = pjoin(here, 'js')
+js_dir = pjoin(here, "js")
 
 # Representative files that should exist after a successful build
 jstargets = [
-    pjoin(js_dir, 'dist', 'index.js'),
+    pjoin(js_dir, "dist", "index.js"),
 ]
 
 data_files_spec = [
-    ('share/jupyter/nbextensions/widget-bandsplot', 'widget_bandsplot/nbextension', '*.*'),
-    ('share/jupyter/labextensions/widget-bandsplot', 'widget_bandsplot/labextension', "**"),
-    ("share/jupyter/labextensions/widget-bandsplot", '.', "install.json"),
-    ('etc/jupyter/nbconfig/notebook.d', '.', 'widget-bandsplot.json'),
+    (
+        "share/jupyter/nbextensions/widget-bandsplot",
+        "widget_bandsplot/nbextension",
+        "*.*",
+    ),
+    (
+        "share/jupyter/labextensions/widget-bandsplot",
+        "widget_bandsplot/labextension",
+        "**",
+    ),
+    ("share/jupyter/labextensions/widget-bandsplot", ".", "install.json"),
+    ("etc/jupyter/nbconfig/notebook.d", ".", "widget-bandsplot.json"),
 ]
 
-cmdclass = create_cmdclass('jsdeps', data_files_spec=data_files_spec)
-cmdclass['jsdeps'] = combine_commands(
-    install_npm(js_dir, npm=['yarn'], build_cmd='build:prod'), ensure_targets(jstargets),
+cmdclass = create_cmdclass("jsdeps", data_files_spec=data_files_spec)
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(js_dir, npm=["yarn"], build_cmd="build:prod"),
+    ensure_targets(jstargets),
 )
 
 TOP_DIR = Path(__file__).resolve().parent
@@ -50,38 +57,40 @@ TOP_DIR = Path(__file__).resolve().parent
 with open(TOP_DIR.joinpath("requirements.txt")) as handle:
     BASE = [f"{_.strip()}" for _ in handle.readlines() if " " not in _]
 
+with open(TOP_DIR.joinpath("requirements_dev.txt")) as handle:
+    DEV = [f"{_.strip()}" for _ in handle.readlines()]
+
 with open(TOP_DIR.joinpath("requirements_tests.txt")) as handle:
     TESTS = [f"{_.strip()}" for _ in handle.readlines()]
 
 setup_args = dict(
     name=name,
     version=version,
-    description='A Jupyter widget to plot bandstructures.',
+    description="A Jupyter widget to plot bandstructures.",
     long_description=LONG_DESCRIPTION,
     include_package_data=True,
     install_requires=BASE,
-    extras_require={"tests": TESTS},
+    extras_require={"dev": DEV, "tests": TESTS},
     packages=find_packages(),
     zip_safe=False,
     cmdclass=cmdclass,
-    author='Dou Du',
-    author_email='dou.du@epfl.ch',
-    url='https://github.com/osscar-org/widget-bandsplot',
+    author="Dou Du",
+    author_email="dou.du@epfl.ch",
+    url="https://github.com/osscar-org/widget-bandsplot",
     keywords=[
-        'ipython',
-        'jupyter',
-        'widgets',
+        "ipython",
+        "jupyter",
+        "widgets",
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: IPython',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'Topic :: Multimedia :: Graphics',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 4 - Beta",
+        "Framework :: IPython",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia :: Graphics",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
 
