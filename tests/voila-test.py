b77b66157bd143d2abf954fe8ea6bb3047ad96c5
Dou Du <dou.du@epfl.ch>
2022-09-07 07:55:15 +0200

    fix the test fails

--- tests/voila-test.py
+++ tests/voila-test.py
@@ -13,7 +13,7 @@ class TestTest01:
         self.driver.quit()
 
     def test_test01(self):
-        self.driver.get("http://localhost:8383/voila/render/index.ipynb")
+        self.driver.get("http://localhost:8383/voila/render/examples/index.ipynb")
         self.driver.set_window_size(1280, 720)
         self.driver.save_screenshot("widget01.png")
         time.sleep(5)
