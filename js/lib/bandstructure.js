3f432c339c966bcf3e8ea80ef20b188f34535e71
Dou Du <56022756+dou-du@users.noreply.github.com>
2022-09-13 16:06:19 +0800

    upgrade chart.js to version 3.2.0 (#28)

--- js/lib/bandstructure.js
+++ js/lib/bandstructure.js
@@ -37,7 +37,7 @@
 // Utility 'zip' function analogous to python's, from
 // https://stackoverflow.com/questions/4856717
 
-const Chart = require('chart.js');
+require('chart.js/dist/chart');
 const { nodeName } = require('jquery');
 var tinycolor = require('tinycolor2');
 require('chartjs-plugin-zoom');
